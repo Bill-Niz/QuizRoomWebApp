5d5df5b6d31102accfedda03119daed4709a1cb7
Bill-Niz <bill.nizeyimana@gmail.com>
2011-12-30 03:27:33 +0100

    next

---
+++ test/web_app_test_suite.rb
@@ -0,0 +1,9 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+$:.unshift File.join(File.dirname(__FILE__),'..','test')
+require 'test/unit' 
+
+# Add your testcases here
+
+require 'test_submit' 
+
